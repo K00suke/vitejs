1e6147ce59060118f129095eaf8cb95b41de804b
K00suke <134691435+K00suke@users.noreply.github.com>
2024-07-25 13:22:33 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<div class="bg-gradient-to-br from-yellow-200 to-red-500 h-svh w-svw">
+    <div class="h-svh max-x-xl flex flex-col gap-4 justify-center">
+        <div class="bg-orange-600">レベル表示</div>
+        <div class="bg-blue-900">スコア表示</div>
+        <div class="bg-green-400 aspect-square">盤面</div>
+        <div class="bg-yellow-500">残り時間</div>
+        <div class="bg-violet-800">ヒントの表示スペース</div>
+    </div>
+</div>
